6399926ffb1eec75c6275d3df9cdbe6320bf7c99
Andre Francelino <andrefgomes.github.version@gmail.com>
2023-10-01 17:29:07 -0300

    Dart - Aula 1

---
+++ aula1-variaveis basicas.dart
@@ -0,0 +1,17 @@
+void main() {
+  int age = 41;
+  String name = "Jared";
+  String surname = 'Padalecki';
+  double height = 1.93;
+  String profession = "Ator";
+  String character = 'Sam Winchester';
+  String seriesName = "Supernatural";
+  String legacy = 'Caçador';
+  int ageStart = 22;
+  int yearStart = 2005;
+  String organizaton = "Warner Bros";
+
+  print('Olá! Eu sou $name $surname.');
+  print( 'Tenho $age anos de idade, $height m de altura, fui $profession no seriado $seriesName, produção de muito sucesso da $organizaton.');
+  print("Interpretei o $legacy $character. No início da série, em $yearStart, minha idade era de $ageStart anos.");
+}
