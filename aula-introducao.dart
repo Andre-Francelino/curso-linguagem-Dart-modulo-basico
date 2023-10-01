4b3cd9c5d13ff1208d65e7892d47e25a38573031
Andre Francelino <andrefgomes.github.version@gmail.com>
2023-10-01 11:44:53 -0300

    Dart - aula 0

---
+++ aula-introducao.dart
@@ -0,0 +1,15 @@
+void main() {
+  // É assim que se comenta uma única linha
+  /*
+      Enquanto é assim
+      que se comenta
+      várias linhas
+      de código. 
+   */
+  var nome = "André";
+  var lang = "Dart";
+  for (var i = 0; i < 100; i++) {
+    print('Hello, $nome. Bem Vindo à linguagem $lang');
+    print('Essa é a interação nº $i nesse loop FOR');
+  }
+}
