c81356a5af00755a5794555eb091d34871634505
Andre Francelino <andrefgomes.github.version@gmail.com>
2023-10-05 11:42:49 -0300

    aula 6

---
+++ aula6-operador ternario.dart
@@ -0,0 +1,10 @@
+void main() {
+  double nota = 6;
+  String mensagem = nota >= 5 ? 'Aprovado' : 'Reprovado';
+  
+  int numero = nota == 5 && nota != 7 ? 10 : 2;
+  
+  print(mensagem);
+  print(numero);
+
+}
