ff05841d32847af70816f651af1ecdb180ca9aa1
Andre Francelino <andrefgomes.github.version@gmail.com>
2023-10-05 16:59:21 -0300

    Dart - Aula 8_while

---
+++ aula8-while)loop 2.dart
@@ -0,0 +1,12 @@
+void main() {
+// imprimir números de 0 a 100 (decrescente)
+  int i = 100;
+
+  while(i >= 0) {
+    print(i);
+    i--;
+  }
+
+  print('Terminou');
+
+}
---
+++ aula8-while)loop.dart
@@ -0,0 +1,12 @@
+void main() {
+// imprimir números de 0 a 100 (crescente)
+  int i = 0;
+
+  while(i <= 100) {
+    print(i);
+    i++; // se não colocar o incremento na variável i o código irá executar infinitamente.
+  }
+
+  print('Terminou');
+
+}
