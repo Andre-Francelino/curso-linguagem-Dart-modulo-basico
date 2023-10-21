552879dfa8785c1a91dd87addc44b98674f653dc
Andre Francelino <andrefgomes.github.version@gmail.com>
2023-10-21 18:49:52 -0300

    Dart - Aula 10_For

---
+++ aula10_1-for_loop.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Loop For primeira forma
+
+  for(int i = 0; i <= 10; i++) {
+    print(i);
+  }
+
+
+}
---
+++ aula10_2-for_loop.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Loop For primeira forma
+  // Um For dentro do outro
+
+  for(int i = 10; i >= 0; i--) {
+    for(int j = 0; j <= 10; j++) {
+      print('i: $i  j: $j');
+    }
+  }
+
+}
